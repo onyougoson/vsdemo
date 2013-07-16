3ec578d6614bf92f05de1c5ce149ce7818d8f6a6
George <gvernon@plymouth.ac.uk>
2013-07-16 14:26:53 +0100

    added Goodbye 3

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Fred!");
-            Console.WriteLine("Goodbye, Bill");
+            Console.WriteLine("Goodbye!");
         }
     }
 }
